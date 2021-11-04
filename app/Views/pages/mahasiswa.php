7dcbdf834eaf5df039974e10976eabdc56b71918
rara542 <ratihnurulaprilianti@gmail.com>
2021-11-04 17:04:00 +0900

    view

---
+++ app/Views/pages/mahasiswa.php
@@ -0,0 +1,20 @@
+<?= $this->extend('template/index'); ?>
+<?= $this->section('content'); ?>
+
+<div class="container">
+    <div class="col-md-12">
+        <div class="row">
+            <table class="table table-dark">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Asal Sekolah</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection(); ?>
